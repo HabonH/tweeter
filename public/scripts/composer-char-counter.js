092a7e34bbbb9dd18135a22dd6609ae03522d10d
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-11-16 20:00:16 -0500

    tweet-container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -11,26 +11,24 @@ inside of the .new-tweet section. */
 
 
 
-$(document).ready(function() {
+$(document).ready(function () {
   //Create variable to keep track of character length
   // Create variable to determine limit of 140
   // create a variable that leads to .counter from tweet-text
-  
-  $("#tweet-text").on( "input", function(event) {
+
+  $("#tweet-text").on("input", function(event) {
     const charNum = $(this).val().length;
     const charLeft = 140 - charNum;
     const counter = $(this).parent().find('.counter');
-    
+
     counter.val(charLeft);
     // CharLeft becomes negative, it should turn red
     // charLeft < 0, conditional
-    if(charLeft < 0){
-      counter.css("color", "red")
+    if (charLeft < 0) {
+      counter.css("color", "red");
     } else {
-      counter.css("color", "black")
+      counter.css("color", "black");
     }
-    
-    // console.log(counter);
-    
+
   });
 });
