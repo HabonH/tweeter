ae15e3e11a12a518c728e5305c84b64635e220d4
Habon Hassan <habonhassan@Habons-MacBook-Air.local>
2021-11-19 00:02:37 -0500

    Final version

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,35 +4,6 @@
 //  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 //  */
 
-// // Send a request to the API --> jQuery
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-
-//   }
-// ];
-
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -59,62 +30,48 @@ const createTweetElement = function (tweetData) {
   return $tweet;
 };
 
-// Time since tweet was first created
 
 const renderTweets = function (tweets) {
   for (const tweet of tweets) {
-    // let $value = createTweetElement(tweet);
     $('#tweets-container').prepend(createTweetElement(tweet));
   }
 };
 
-
-
-// Handler for .ready() called.
 $(document).ready(function () {
   function loadtweets() {
 
-    $.get('/tweets').then(function(data) {
+    $.get('/tweets').then(function (data) {
       renderTweets(data);
-      // console.log("SUCCESS!! ");
     });
-    // loadtweets();
-
   };
 
   $('#data').on('submit', function (event) {
     event.preventDefault();
     $(".error-msg").hide();
+
     if ($('#tweet-text').val().length === 0) {
-      // return alert("Sorry, your tweet has no content. How will others know what you're humming?");
-      // $(".error-msg").slideDown("slow", function () {
-        
-        //   return $(`Error- field is empty`);
-        // });
-        $(".error-msg")
+      $(".error-msg")
         .html("🛑 Your tweet has no content. How will others know what you're humming?")
         .slideDown();
-      }
-      if ($('#tweet-text').val().length > 140) {
-        // return alert("Your tweet content is too long");
-        $(".error-msg")
-        .html("🛑 Oh no! Your tweet content is too long. Don't worry, you can try again.")
-        .slideDown();
-        
-      } else {
-        const $value = $(this).serialize();
-        $.post('/tweets', $value).then(function () {
-          loadtweets();
-          $("#tweet-text").val("");
-          $(".counter").val(140);
-      });
     }
 
+    if ($('#tweet-text').val().length > 140) {
+      $(".error-msg")
+        .html("🛑 Oh no! Your tweet content is too long. Don't worry, you can try again.")
+        .slideDown();
+
+    } else {
+      const $value = $(this).serialize();
+      $.post('/tweets', $value).then(function () {
+        loadtweets();
+        $("#tweet-text").val("");
+        $(".counter").val(140);
+
+      });
+    }
   });
 });
 
 
 
 
-// console.log("new DATA--->", ($('.tweets-container'))); // to see what it looks like
-// console.log("NEW TWEET -->", $value); // to see what it looks like
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,34 +1,17 @@
-/* Using jQuery and an appropriate selector, register an 
-event handler to the textarea element for the form
-inside of the .new-tweet section. */
-// Selector is <textarea> inside form
-// Element is id="tweet-text"
-
-
-// Define the element
-// const textInput = document.getElementsById('tweet-text');
-// const textArea = document.querySelector('textarea')
-
-
-
 $(document).ready(function () {
-  //Create variable to keep track of character length
-  // Create variable to determine limit of 140
-  // create a variable that leads to .counter from tweet-text
 
-  $("#tweet-text").on("input", function(event) {
+  $("#tweet-text").on("input", function (event) {
     const charNum = $(this).val().length;
     const charLeft = 140 - charNum;
     const counter = $(this).parent().find('.counter');
 
     counter.val(charLeft);
-    // CharLeft becomes negative, it should turn red
-    // charLeft < 0, conditional
+
     if (charLeft < 0) {
       counter.css("color", "red");
     } else {
       counter.css("color", "black");
-      
+
     }
 
   });
